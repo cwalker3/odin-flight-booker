ea4184cdc6a7b0c025f7e1dd369403fb6d137f0e
Cory Walker <cory.walker.dev@gmail.com>
2023-05-23 13:52:03 -0700

    Add gmail smtp for sending emails in production

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,6 +1,7 @@
 class PassengerMailer < ApplicationMailer
   def booking_confirmation(passenger)
     @passenger = passenger
+    @booking = passenger.booking
     @flight = @passenger.flight
     mail(to: @passenger.email, subject: 'Flight Booking Confirmation')
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,21 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            Rails.application.credentials.gmail_smtp.email,
+  password:             Rails.application.credentials.gmail_smtp.password,
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
 
 end
